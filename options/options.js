25d0a40e084ddbd99c9b5e614e76c8e0f20a0ae6
Crowdbotics <team@crowdbotics.com>
2024-11-21 15:53:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wavelength-50111",
+  url: "https://wavelength-50111.botics.co",
+  api: "https://wavelength-50111.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
